37cacebd7095fa71a3a6331594048801683c512e
Entrivax <entrivax@gmail.com>
2016-12-10 15:52:56 +0100

    Some files added

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+    public float speed = 10f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        float dir = Input.GetAxis("Horizontal");
+        rb.velocity += new Vector2(dir, 0) * speed;
+	}
+}
